b83987a3462449c3b1287e42f88477b90dd0d53f
germashukanna <germashuk@gmail.com>
2022-06-02 14:39:05 +0300

    edits after review 01-2

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,18 +6,18 @@ export const Footer = () => {
     return (
         <div className={s.container}>
             <h2>Гермашук Анна</h2>
-            {/*<ul className={s.list}>*/}
-            {/*    <li className={s.listFooter}><a><img></img><span></span></a></li>*/}
-            {/*    <li className={s.listFooter}><a><img></img><span></span></a></li>*/}
-            {/*    <li className={s.listFooter}><a><img></img><span></span></a></li>*/}
-            {/*    <li className={s.listFooter}><a><img></img><span></span></a></li>*/}
-            {/*</ul>*/}
-            <div className={s.list}>
-                <span className={s.listFooter}><a><img></img></a></span>
-                <span className={s.listFooter}><a><img></img></a></span>
-                <span className={s.listFooter}><a><img></img></a></span>
-                <span className={s.listFooter}><a><img></img></a></span>
-            </div>
+            <ul className={s.list}>
+                <li className={s.listFooter}><a><img></img><span></span></a></li>
+                <li className={s.listFooter}><a><img></img><span></span></a></li>
+                <li className={s.listFooter}><a><img></img><span></span></a></li>
+                <li className={s.listFooter}><a><img></img><span></span></a></li>
+            </ul>
+            {/*<div className={s.list}>*/}
+            {/*    <span className={s.listFooter}><a><img></img></a></span>*/}
+            {/*    <span className={s.listFooter}><a><img></img></a></span>*/}
+            {/*    <span className={s.listFooter}><a><img></img></a></span>*/}
+            {/*    <span className={s.listFooter}><a><img></img></a></span>*/}
+            {/*</div>*/}
             <div className={s.footerEhd}><h4>© 2022 Все права защищены</h4></div>
         </div>
     )
